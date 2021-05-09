c04485807fee7cf07da3f399c4d9bcc8e8e120f1
gowtham2910r <55842025+gowtham2910r@users.noreply.github.com>
2021-05-09 11:22:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function compute()
     var principal = document.getElementById("principal").value;
     if(principal <= 0){
         alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
     }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;

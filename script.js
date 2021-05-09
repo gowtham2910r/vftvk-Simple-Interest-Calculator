7153be600e1cf6e00eb173f8c62268149cfba99d
gowtham2910r <55842025+gowtham2910r@users.noreply.github.com>
2021-05-09 11:06:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+//function to compute amount
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    if(principal <= 0){
+        alert("Enter a positive number");
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate/100;
+    var year =  new Date().getFullYear()+parseInt(years);
+    
+    document.getElementById("result").innerHTML="If you deposit <strong>"+principal+"</strong>,\<br\>at an interest rate of <strong>"+rate+"%</strong>\<br\>You will receive an amount of <strong>"+interest+"</strong>,\<br\>in the year <strong>"+year+"</strong>\<br\>"
+
     
 }
+//function to update rate
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+    
+        
         
